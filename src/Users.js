e8aad6bb5ea2b68e2fb8dc056c749b4a6f511909
탁현지 <taghyeonji@taghyeonjiui-MacBookPro.local>
2020-12-27 22:47:46 +0900

    useReducer 사용

--- src/Users.js
+++ src/Users.js
@@ -1,31 +1,57 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import axios from 'axios';
 
+// LOADING, SUCCESS, ERROR
+function reducer(state , action){
+    switch (action.type) {
+        case 'LOADING':
+            return {
+                loading: true,
+                data: null,
+                error: null
+            };
+        case 'SUCCESS':
+            return {
+                loading: false,
+                data: action.data,
+                error: null
+            };
+        case 'ERROR':
+            return {
+                loading: false,
+                data: null,
+                error: action.error
+            };
+        default:
+            throw new Error(`Unhandled action type: ${action.type}`);
+    }
+}
+
 function Users() {
-    const [users, setUsers] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
 
+    const [state, dispatch] = useReducer(reducer, {
+        //초기값 설정 
+        loading : false,
+        data : null,
+        error : null
 
-    //처음 렌더링 될때 어떤 작업을 하겠다.
+    })
+
+    const fetchUsers = async () => {
+        dispatch({type : 'LOADING'});
+        try {
+            const response = await axios.get('https://jsonplaceholder.typicode.com/users/');
+            dispatch({type : 'SUCCESS' , data : response.data });
+        } catch (e) {
+            dispatch({type : 'ERROR' , error : e });
+        }
+    }
+
     useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                // 초기화
-                setUsers(null);
-                setError(null);
-                setLoading(true);
-                const response = await axios.get('https://jsonplaceholder.typicode.com/users/');
-                
-                setUsers(response.data);
-            } catch (e) {
-                setError(e);
-            }
-            setLoading(false);
-        };
         fetchUsers();
     }, []);
 
+    const {loading, data : users, error} = state;
     if (loading) return <div> 로딩중..</div>
     if (error) return <div> 에러발생..</div>
     if (!users) return null;
