9709938722136df0d5f6bb436fd0c0518a1f4e36
탁현지 <taghyeonji@taghyeonjiui-MacBookPro.local>
2020-12-27 22:20:50 +0900

    api 호출 테스트

---
+++ src/Users.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Users() {
+    const [users, setUsers] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+
+    //처음 렌더링 될때 어떤 작업을 하겠다.
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                // 초기화
+                setUsers(null);
+                setError(null);
+                setLoading(true);
+                const response = await axios.get('https://jsonplaceholder.typicode.com/users/');
+                
+                setUsers(response.data);
+            } catch (e) {
+                setError(e);
+            }
+            setLoading(false);
+        };
+        fetchUsers();
+    }, []);
+
+    if (loading) return <div> 로딩중..</div>
+    if (error) return <div> 에러발생..</div>
+    if (!users) return null;
+
+    return <ul>
+        {users.map(user => <li key={user.id}>
+            {user.username} ({user.name})
+
+        </li>)}
+
+
+    </ul>;
+
+}
+
+export default Users;
